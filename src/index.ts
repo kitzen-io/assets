0abbe96355a7d73c7b6d7f9316d4259cec9a676d
akoidan <deathangel908@gmail.com>
2023-09-21 01:24:13 +0200

    Fixed order

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,18 @@ import assetTestMetadataObject from './assets.test.metadata.constant.json';
 import defaultAssets from './default.asssets.json';
 
 function getAssetMetadata(env: 'production' | 'development' = 'production'): IAssetMetadata[] {
-  if (env === 'production') {
-    return assetsMetadata;
-  } else {
+  if (env === 'development') {
     return assetsTestMetadata;
+  } else {
+    return assetsMetadata;
   }
 }
 
 function getAssetMetadataObject(env: 'production' | 'development' = 'production'):  { [network: string]: { [identifier: string]: IAssetMetadata } } {
-  if (env === 'production') {
-    return assetMetadataObject;
-  } else {
+  if (env === 'development') {
     return assetTestMetadataObject;
+  } else {
+    return assetMetadataObject;
   }
 }
 
