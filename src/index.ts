f9634540953e406525806e93ac79dd42f41bca5b
akoidan <deathangel908@gmail.com>
2023-09-20 15:03:40 +0200

    Multienv tron

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import IAssetMetadata from './interface/asset-metadata.interface';
 import assetsMetadata from './assets.metadata.json';
 import assetMetadataObject from './assets.metadata.constant.json';
+import defaultAssets from './default.asssets.json';
 
 const AssetsMetadata: IAssetMetadata[] = assetsMetadata;
 const AssetsMetadataObject: { [network: string]: { [identifier: string]: IAssetMetadata } } = assetMetadataObject;
@@ -8,5 +9,6 @@ const AssetsMetadataObject: { [network: string]: { [identifier: string]: IAssetM
 export {
   AssetsMetadata,
   AssetsMetadataObject,
+  defaultAssets,
   IAssetMetadata,
 };
