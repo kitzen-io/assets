8821d34fed7f89a5af368348f3af81f56ad388f8
akoidan <deathangel908@gmail.com>
2023-09-21 01:04:31 +0200

    Fix

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,33 @@
 import IAssetMetadata from './interface/asset-metadata.interface';
 import assetsMetadata from './assets.metadata.json';
+import assetsTestMetadata from './assets.test.metadata.json';
 import assetMetadataObject from './assets.metadata.constant.json';
+import assetTestMetadataObject from './assets.test.metadata.constant.json';
 import defaultAssets from './default.asssets.json';
 
-const AssetsMetadata: IAssetMetadata[] = assetsMetadata;
-const AssetsMetadataObject: { [network: string]: { [identifier: string]: IAssetMetadata } } = assetMetadataObject;
+const AssetsMetadataObject: = assetMetadataObject;
+
+function getAssetMetadata(env: 'prod' | 'test' = 'prod'): IAssetMetadata[] {
+  if (env === 'prod') {
+    return assetsMetadata;
+  } else {
+    return assetsTestMetadata;
+  }
+}
+
+function getAssetMetadataObject(env: 'prod' | 'test' = 'prod'):  { [network: string]: { [identifier: string]: IAssetMetadata } } {
+  if (env === 'prod') {
+    return assetMetadataObject;
+  } else {
+    return assetsTestMetadata;
+  }
+}
+
+
 
 export {
-  AssetsMetadata,
-  AssetsMetadataObject,
+  getAssetMetadata,
+  getAssetMetadataObject,
   defaultAssets,
   IAssetMetadata,
 };
