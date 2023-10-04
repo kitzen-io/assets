2112b9accdf6c1a56d66013c6ad1b6090b6086f1
neese <temanharosh@gmail.com>
2023-10-04 17:57:05 +0300

    rename flags to currency name, add function to get language icon

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import assetMetadataObject from './assets.metadata.constant.json';
 import assetTestMetadataObject from './assets.test.metadata.constant.json';
 import defaultAssets from './default.asssets.json';
 import { IAssetMetadataObject } from './interface/asset-metadata-object.interface';
+import { Countries } from './countries';
 
 function getAssetMetadata(env: 'production' | 'development' = 'production'): IAssetMetadata[] {
   if (env === 'development') {
@@ -22,9 +23,14 @@ function getAssetMetadataObject(env: 'production' | 'development' = 'production'
   }
 }
 
+function getLanguageIconName(country: string): string {
+  return `${Countries[country]}.svg`;
+}
+
 export {
   getAssetMetadata,
   getAssetMetadataObject,
+  getLanguageIconName,
   defaultAssets,
   IAssetMetadata,
   IAssetMetadataObject,
